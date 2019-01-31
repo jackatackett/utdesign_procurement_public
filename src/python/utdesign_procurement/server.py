0617a5fcd0b71a7b86344742469d659a2d87c544
Xander Wong <xanxerus@gmail.com>
2019-01-30 18:35:17 -0600

    MORE path nonsense

--- src/python/utdesign_procurement/server.py
+++ src/python/utdesign_procurement/server.py
@@ -24,7 +24,9 @@ def main():
     cherrypy.Application.wwwDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
         os.path.join('..', '..', 'www'))
 
-    server_config = os.path.abspath(os.path.join('..', '..', 'etc', 'server.conf'))
+    server_config = os.path.abspath(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 
+        '..', '..', 'etc', 'server.conf'))
     cherrypy.tree.mount(Root(), '/', config=server_config)
     cherrypy.engine.start()
     cherrypy.engine.block()
