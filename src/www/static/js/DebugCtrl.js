fffe23a184889f100692a48285482ea6c639189f
Xander Wong <xanxerus@gmail.com>
2019-02-11 16:23:34 -0600

    Made debug app better

--- src/www/static/js/DebugCtrl.js
+++ src/www/static/js/DebugCtrl.js
@@ -3,14 +3,17 @@ app.controller('DebugCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.data = "Input goes here";
     $scope.output = "Output goes here";
     $scope.url = "procurementRequest";
+    $scope.status = "#FFF"
 
     $scope.makeRequest = function() {
         console.log("makeRequest", $scope.data);
-        $http.post($scope.url, JSON.parse($scope.data)).then(function(data) {
+        $http.post($scope.url, $scope.data).then(function(data) {
             console.log(data);
-            $scope.output = "Success: " + JSON.stringify(data);
+            $scope.status = "green";
+            $scope.output = JSON.stringify(data);
         }, function(err) {
             console.log(err);
+            $scope.status = "red";
             $scope.output = "Error: " + JSON.stringify(err);
         });
     }
