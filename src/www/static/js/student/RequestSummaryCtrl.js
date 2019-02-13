aa3affaa679bf16e73f0b4ccf7395b848c53d66d
Jack Tackett <jackatackett@gmail.com>
2019-02-13 02:12:00 -0600

    created mockup frontend for student summary page

--- src/www/static/js/student/RequestSummaryCtrl.js
+++ src/www/static/js/student/RequestSummaryCtrl.js
@@ -1,30 +1,55 @@
 app.controller('RequestSummaryCtrl', ['$scope', '$http', function($scope, $http) {
 
+    $scope.fieldKeys = ["groupID", "status", "vendor", "URL", "justification", "additionalInfo"];
     $scope.fields = ["Group ID", "Status", "Vendor", "URL", "Justification", "Additional Info"];
     $scope.grid = [];
+    $scope.itemFieldKeys = ["description", "partNo", "quantity", "unitCost", "total"];
+    $scope.itemFields = ["Description", "Catalog Part Number", "Quantity", "Estimated Unit Cost", "Total Cost"];
 
-    $scope.addRow = function() {
-        $scope.grid.push(newRow());
-    }
+    $scope.data = [ {
+                        groupID: "123",
+                        status: "pending",
+                        vendor: "Home Depot",
+                        URL: "homedepot.com",
+                        justification: "Because he told me toooooo",
+                        additionalInfo: "He is Plankton",
+                        items: [ {
+                                description: "A big thing",
+                                partNo: "9001",
+                                quantity: "25",
+                                unitCost: "1000000000",
+                                total: "25000000000"
+                            },
+                            {
+                                description: "A small thing",
+                                partNo: "9002",
+                                quantity: "250",
+                                unitCost: "10",
+                                total: "2500"
+                            }
+                        ]
+                    },
+                    {
+                        groupID: "124",
+                        status: "approved",
+                        vendor: "The Plastic Store",
+                        URL: "gmail.com",
+                        justification: "O Captain, My Captain",
+                        additionalInfo: "He is dead, Jim",
+                        items: [ {
+                                description: "A hunk of plastic",
+                                partNo: "9003",
+                                quantity: "1",
+                                unitCost: "10",
+                                total: "10"
+                            }
+                        ]
+                    }
+                  ]
 
-    $scope.makeRequest = function() {
-        console.log("makeRequest", $scope.grid);
-        $http.post('/procurementRequest', $scope.grid, function(data) {
-            console.log("Success", data)
-        }, function(err) {
-            console.error("Error", err)
-        })
-    }
+    $scope.toggleCollapse = function(e) {
+        var target = e.currentTarget;
+        $(target.nextElementSibling).toggle();
+    };
 
-    function newRow() {
-        var ret = {};
-        for (var x = 0; x < $scope.fields.length; x++) {
-            ret[$scope.fields[x]] = "";
-        }
-        return ret;
-    }
-
-    $(document).ready(function() {
-        $scope.addRow();
-    });
 }]);
