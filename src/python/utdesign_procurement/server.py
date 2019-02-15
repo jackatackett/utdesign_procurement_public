149b42b9a1f14f58a2b5664511bef18eaa4e77e0
Xander Wong <xanxerus@gmail.com>
2019-02-15 17:55:42 -0600

    Simplified server.py back to how it was before this branch was made

--- src/python/utdesign_procurement/server.py
+++ src/python/utdesign_procurement/server.py
@@ -15,6 +15,13 @@ class Root(object):
         cherrypy.log("Template Dir: %s" % templateDir)
         self.templateLookup = TemplateLookup(directories=templateDir)
 
+        client = pm.MongoClient()
+        db = client['procurement']
+        self.colRequests = db['requests']
+        self.colUsers = db['users']
+
+    # Pages go below
+
     @cherrypy.expose
     def index(self):
         template = self.templateLookup.get_template('LoginApp.html')
@@ -22,17 +29,33 @@ class Root(object):
         cherrypy.log(str(type(ret)))
         return ret
 
-class API(object):
 
-    def __init__(self):
-        templateDir = os.path.join(cherrypy.Application.wwwDir, 'templates')
-        cherrypy.log("Template Dir: %s" % templateDir)
-        self.templateLookup = TemplateLookup(directories=templateDir)
+    @cherrypy.expose
+    def student(self):
+        template = self.templateLookup.get_template('student/StudentApp.html')
+        ret = template.render()
+        return ret
 
-        client = pm.MongoClient()
-        db = client['procurement']
-        self.colRequests = db['requests']
-        self.colUsers = db['users']
+    @cherrypy.expose
+    def manager(self):
+        template = self.templateLookup.get_template('manager/ManagerApp.html')
+        ret = template.render()
+        return ret
+
+    @cherrypy.expose
+    def admin(self):
+        template = self.templateLookup.get_template('admin/ AdminApp.html')
+        ret = template.render()
+        return ret
+
+    @cherrypy.expose
+    def debug(self):
+        template = self.templateLookup.get_template('debug/DebugApp.html')
+        ret = template.render()
+        cherrypy.log(str(type(ret)))
+        return ret
+
+    # API Functions go below
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -492,59 +515,6 @@ class API(object):
         else:
             raise cherrypy.HTTPError(400, 'data needs object id')
 
-class StudentView:
-
-    def __init__(self):
-        templateDir = os.path.join(cherrypy.Application.wwwDir, 'templates')
-        cherrypy.log("Template Dir: %s" % templateDir)
-        self.templateLookup = TemplateLookup(directories=templateDir)
-
-    @cherrypy.expose
-    def index(self):
-        template = self.templateLookup.get_template('student/StudentApp.html')
-        ret = template.render()
-        return ret
-
-class ManagerView:
-
-    def __init__(self):
-        templateDir = os.path.join(cherrypy.Application.wwwDir, 'templates')
-        cherrypy.log("Template Dir: %s" % templateDir)
-        self.templateLookup = TemplateLookup(directories=templateDir)
-
-    @cherrypy.expose
-    def index(self):
-        template = self.templateLookup.get_template('manager/ManagerApp.html')
-        ret = template.render()
-        return ret
-
-class AdminView:
-
-    def __init__(self):
-        templateDir = os.path.join(cherrypy.Application.wwwDir, 'templates')
-        cherrypy.log("Template Dir: %s" % templateDir)
-        self.templateLookup = TemplateLookup(directories=templateDir)
-
-    @cherrypy.expose
-    def index(self):
-        template = self.templateLookup.get_template('admin/ AdminApp.html')
-        ret = template.render()
-        return ret
-
-class DebugView:
-
-    def __init__(self):
-        templateDir = os.path.join(cherrypy.Application.wwwDir, 'templates')
-        cherrypy.log("Template Dir: %s" % templateDir)
-        self.templateLookup = TemplateLookup(directories=templateDir)
-
-    @cherrypy.expose
-    def index(self):
-        template = self.templateLookup.get_template('debug/DebugApp.html')
-        ret = template.render()
-        cherrypy.log(str(type(ret)))
-        return ret
-
 def main():
     cherrypy.Application.wwwDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
         os.path.join('..', '..', 'www'))
@@ -553,11 +523,6 @@ def main():
         os.path.dirname(os.path.realpath(__file__)), 
         '..', '..', 'etc', 'server.conf'))
     cherrypy.tree.mount(Root(), '/', config=server_config)
-    cherrypy.tree.mount(API(), '/api', config=server_config)
-    cherrypy.tree.mount(StudentView(), '/student', config=server_config)
-    cherrypy.tree.mount(ManagerView(), '/manager', config=server_config)
-    cherrypy.tree.mount(AdminView(), '/admin', config=server_config)
-    cherrypy.tree.mount(DebugView(), '/debug', config=server_config)
 
     cherrypy.engine.start()
     input()
