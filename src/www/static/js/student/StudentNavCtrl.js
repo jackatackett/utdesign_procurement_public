37da2f05cff9251ad7567f3203664a543a90ce76
Marcus D <MarcusD.9234@gmail.com>
2019-02-20 18:37:33 -0600

    added minimal budget display for student view

--- src/www/static/js/student/StudentNavCtrl.js
+++ src/www/static/js/student/StudentNavCtrl.js
@@ -11,18 +11,26 @@ app.controller('StudentNavCtrl', ['$scope', '$location', function($scope, $locat
             $("#createRequestCtrlDiv").show();
             $("#requestSummaryCtrlDiv").hide();
             $("#studentHelpDiv").hide();
-
+            $("#viewBudgetDiv").hide();
         } else if (hash == 'status') {
             //show status
             $("#createRequestCtrlDiv").hide();
             $("#requestSummaryCtrlDiv").show();
             $("#studentHelpDiv").hide();
-
-        } else {
+            $("#viewBudgetDiv").hide();
+        } else if (hash == 'budget') {
+            //show budget
+            $("#createRequestCtrlDiv").hide();
+            $("#requestSummaryCtrlDiv").hide();
+            $("#studentHelpDiv").hide();
+            $("#viewBudgetDiv").show();
+        }
+        else {
             //show help
             $("#createRequestCtrlDiv").hide();
             $("#requestSummaryCtrlDiv").hide();
             $("#studentHelpDiv").show();
+            $("#viewBudgetDiv").hide();
         }
 
         console.log("moo", $location.hash());
