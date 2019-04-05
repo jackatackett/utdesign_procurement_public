fdbaffd1699b518011ae4fe5951b710c4db6286e
MonicaNeivandt <msneivandt@gmail.com>
2019-04-05 16:24:48 -0500

    Moved Justification/AdditionalInfo to drop down table Added Total cost and Shipping Cost in their place in summary tables for student and admin

--- src/www/static/js/student/RequestSummaryCtrl.js
+++ src/www/static/js/student/RequestSummaryCtrl.js
@@ -1,7 +1,6 @@
 app.controller('RequestSummaryCtrl', ['$scope', '$http', '$location', '$timeout', '$interval', 'dispatcher', function($scope, $http, $location, $timeout, $interval, dispatcher) {
 
     function convertCosts(value) {
-//        console.log(value);
         if (typeof value === "undefined") {
             return "$0.00";
         }
@@ -23,56 +22,19 @@ app.controller('RequestSummaryCtrl', ['$scope', '$http', '$location', '$timeout'
                 result[d]["items"][item]["unitCost"] = convertCosts(data[d]["items"][item]["unitCost"]);
                 result[d]["items"][item]["totalCost"] = convertCosts(data[d]["items"][item]["totalCost"]);
             }
+            result[d]["requestTotal"] = convertCosts(data[d]["requestTotal"]);
+            result[d]["shippingCost"] = convertCosts(data[d]["shippingCost"]);
         }
         return result;
     };
 
-    $scope.fieldKeys = ["requestNumber", "projectNumber", "manager", "status", "vendor", "URL", "justification", "additionalInfo"];
-    $scope.fields = ["Request Number", "Project Number", "Assigned Manager Email", "Status", "Vendor", "URL", "Justification", "Additional Info"];
+    $scope.fieldKeys = ["requestNumber", "projectNumber", "manager", "status", "vendor", "URL", "requestTotal", "shippingCost"];
+    $scope.fields = ["Request Number", "Project Number", "Assigned Manager Email", "Status", "Vendor", "URL", "Total Cost", "Shipping Cost"];
     $scope.grid = [];
     $scope.itemFieldKeys = ["description", "itemURL", "partNo", "quantity", "unitCost", "totalCost"];
     $scope.itemFields = ["Description", "Item URL", "Catalog Part Number", "Quantity", "Estimated Unit Cost", "Total Cost"];
 
-    $scope.data = [ {
-                        projectNumber: 123,
-                        status: "pending",
-                        vendor: "Home Depot",
-                        URL: "homedepot.com",
-                        justification: "Because he told me toooooo",
-                        additionalInfo: "He is Plankton",
-                        items: [ {
-                                description: "A big thing",
-                                partNo: "9001",
-                                quantity: "25",
-                                unitCost: "1000000000",
-                                total: "25000000000"
-                            },
-                            {
-                                description: "A small thing",
-                                partNo: "9002",
-                                quantity: "250",
-                                unitCost: "10",
-                                total: "2500"
-                            }
-                        ]
-                    },
-                    {
-                        projectNumber: 124,
-                        status: "approved",
-                        vendor: "The Plastic Store",
-                        URL: "gmail.com",
-                        justification: "O Captain, My Captain",
-                        additionalInfo: "He is dead, Jim",
-                        items: [ {
-                                description: "A hunk of plastic",
-                                partNo: "9003",
-                                quantity: "1",
-                                unitCost: "10",
-                                total: "10"
-                            }
-                        ]
-                    }
-                  ]
+    $scope.data = [];
 
     $scope.toggleCollapse = function(e) {
         var target = e.currentTarget;
@@ -112,7 +74,6 @@ app.controller('RequestSummaryCtrl', ['$scope', '$http', '$location', '$timeout'
 
     $scope.refreshStatuses = function() {
         $http.post('/procurementStatuses', {}).then(function(resp) {
-            console.log("procurementStatuses success", resp)
             $scope.data = cleanData(resp.data);
         }, function(err) {
             console.error("Error", err.data)
@@ -128,14 +89,10 @@ app.controller('RequestSummaryCtrl', ['$scope', '$http', '$location', '$timeout'
     $scope.historyFieldKeys = ["timestamp", "actor", "comment", "oldState", "newState"];
 
     $scope.viewHistory = function(e, rowIdx) {
-        console.log("history");
-        //~ console.log($scope.data[rowIdx]);
-        //~ console.log($scope.data[rowIdx]["history"]);
         $("#historyBody").empty();
         var historyHTML = "";
         for (var hist in $scope.data[rowIdx]["history"]) {
             historyHTML += '<tr style="border-bottom: 1.5px solid black">';
-                //~ console.log($scope.data[rowIdx]["history"][hist]);
             for (var ele in $scope.historyFieldKeys) {
                 historyHTML += '<td scope="col" style="background-color: #eee;">' + $scope.data[rowIdx]["history"][hist][$scope.historyFieldKeys[ele]] + '</td>';
             }
@@ -145,7 +102,6 @@ app.controller('RequestSummaryCtrl', ['$scope', '$http', '$location', '$timeout'
             historyHTML = "No history";
         }
         $("#historyBody").append(historyHTML);
-        console.log(historyHTML);
         $("#historyModal").show();
     };
 
