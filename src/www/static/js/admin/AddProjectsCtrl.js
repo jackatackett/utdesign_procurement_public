de9710a22d7857201f9d5ddae9ef2a4daf7abe11
MonicaNeivandt <msneivandt@gmail.com>
2019-04-14 00:55:34 -0500

    Add Projects set default budget Made emails optional

--- src/www/static/js/admin/AddProjectsCtrl.js
+++ src/www/static/js/admin/AddProjectsCtrl.js
@@ -1,24 +1,35 @@
 app.controller('AddProjectsCtrl', ['$scope', '$location', '$http', '$window', function($scope, $location, $http, $window) {
 
     $scope.errorText = "";
-    $scope.fieldKeys = ["projectNumber", "sponsorName", "projectName", "membersEmails", "defaultBudget"];
-    $scope.fields = ["Project Number", "Sponsor Name", "Project Name", "Members Emails", "Budget"];
+    $scope.fieldKeys = ["projectNumber", "sponsorName", "projectName", "membersEmails"];
+    $scope.fields = ["Project Number", "Sponsor Name", "Project Name", "Members Emails"];
     $scope.grid = [];
     $scope.itemFieldKeys = ["description", "partNo", "quantity", "unitCost", "total"];
     $scope.itemFields = ["Description", "Catalog Part Number", "Quantity", "Estimated Unit Cost", "Total Cost"];
     $scope.columns = ["Project Number(s)", "First Name", "Last Name", "NetID", "Email", "Course"];
-    $scope.projectInfo = {};
+    $scope.projectInfo = {'defaultBudget':'2000.00'};
     $scope.projectNumArray = [];
+    $scope.membersEmails = [];
 
-    $scope.users = []
+    $scope.users = [];
 
     $scope.addProject = function(e) {
         var target = e.currentTarget;
 
+        if($scope.projectInfo.membersEmails) {
+            $scope.membersEmails = $scope.projectInfo.membersEmails.replace(' ', '').split(',');
+        }
+
+        if($scope.projectInfo.defaultBudget) {
+            if(!($scope.projectInfo.defaultBudget.includes("."))) {
+                $scope.projectInfo.defaultBudget = $scope.projectInfo.defaultBudget + ".00";
+            }
+        }
+
         if(!validateRequest()) {
             console.log("request was invalid");
         } else {
-            $http.post('/projectAdd', {'projectNumber':Number($scope.projectInfo.projectNumber), 'sponsorName':$scope.projectInfo.sponsorName, 'projectName':$scope.projectInfo.projectName, 'membersEmails':$scope.projectInfo.members, 'defaultBudget':Number($scope.projectInfo.defaultBudget), 'availableBudget':Number($scope.projectInfo.defaultBudget), 'pendingBudget':Number($scope.projectInfo.defaultBudget)}).then(function(resp) {
+            $http.post('/projectAdd', {'projectNumber':Number($scope.projectInfo.projectNumber), 'sponsorName':$scope.projectInfo.sponsorName, 'projectName':$scope.projectInfo.projectName, 'membersEmails':$scope.membersEmails, 'defaultBudget':$scope.projectInfo.defaultBudget, 'availableBudget':$scope.projectInfo.defaultBudget, 'pendingBudget':$scope.projectInfo.defaultBudget}).then(function(resp) {
                 console.log("Success", resp);
             }, function(err) {
                 console.error("Error", err.data);
@@ -69,18 +80,6 @@ app.controller('AddProjectsCtrl', ['$scope', '$location', '$http', '$window', fu
     function validateRequest(){
         var valid = false;
 
-//        $http.post('/projectValidate', {'projectNumbers': $scope.projectNumArray}).then(function(resp) {
-//            if(resp.data === "true") {
-//                valid = true;
-//            }
-//            if (valid === false) {
-//                $scope.errorText = "Invalid Project Number";
-//                return false;
-//            }
-//        }, function(err) {
-//            console.error("project validate fail", err)
-//        });
-
         if(!$scope.projectInfo.sponsorName || $scope.projectInfo.sponsorName.length <= 0) {
             $scope.errorText = "Sponsor name must not be empty";
             return false;
@@ -88,11 +87,6 @@ app.controller('AddProjectsCtrl', ['$scope', '$location', '$http', '$window', fu
 
         if(!$scope.projectInfo.projectName || $scope.projectInfo.projectName.length <= 0) {
             $scope.errorText = "Project name must not be empty";
-            return false;
-        }
-
-        if(!$scope.projectInfo.membersEmails || $scope.projectInfo.membersEmails.length <= 0) {
-            $scope.errorText = "Members emails must not be empty";
             return false;
         }
 
